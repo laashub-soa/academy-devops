6ae957b03a6f96cbec906cb31babbbfaf165cd92
root <root@ip-172-31-15-212.us-west-2.compute.internal>
2020-03-24 22:29:32 +0000

    still trying

--- cluster/usr/bin/streamsets_start.sh
+++ cluster/usr/bin/streamsets_start.sh
@@ -1,4 +1,4 @@
-git pull https://github.com/streamsets/academy-devops.git
+git -C /root/academy-devops pull https://github.com/streamsets/academy-devops.git
 cp /root/academy-devops/cluster/home/ubuntu/*_docker.sh /home/ubuntu
 chown ubuntu: /home/ubuntu/*_docker.sh
 su - ubuntu -c "/bin/bash /home/ubuntu/start_docker.sh"

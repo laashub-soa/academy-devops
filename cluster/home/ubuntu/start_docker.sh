699ac4320cfa462a21d738d6f4d153131595e9ce
root <root@ip-172-31-15-212.us-west-2.compute.internal>
2020-03-24 22:01:36 +0000

    Trying out different settings because the temp_credentials file isn't being output

--- cluster/home/ubuntu/start_docker.sh
+++ cluster/home/ubuntu/start_docker.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Pull the customer id from the tags and default to either user parameter or "generic" if the tag isn't set
 EC2_AVAIL_ZONE="`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`"
--- cluster/usr/bin/streamsets_start.sh
+++ cluster/usr/bin/streamsets_start.sh
@@ -1,4 +1,4 @@
 git pull https://github.com/streamsets/academy-devops.git
 cp /root/academy-devops/cluster/home/ubuntu/*_docker.sh /home/ubuntu
 chown ubuntu: /home/ubuntu/*_docker.sh
-su - ubuntu -c "/home/ubuntu/start_docker.sh"
+su - ubuntu -c "/bin/bash /home/ubuntu/start_docker.sh"

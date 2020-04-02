dfa33258dc96b9bfc8a11476d57b935908c5953a
root <root@ip-172-31-15-212.us-west-2.compute.internal>
2020-04-02 00:41:44 +0000

    adding node-3 transformer IP auto config

--- cluster/home/ubuntu/start_docker.sh
+++ cluster/home/ubuntu/start_docker.sh
@@ -11,9 +11,13 @@ EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
 
 EC2_INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`"
 
-TAG="`aws ec2 describe-tags --output text --region \"$EC2_REGION\" --filters \"Name=key,Values=customername\" \"Name=resource-id,Values=$EC2_INSTANCE_ID\"`"
+EC2_PUBLIC_IP="`wget -q -O - http://169.254.169.254/latest/meta-data/public-ipv4`"
 
-CUSTOMER="`echo $TAG | cut -d' ' -f 5`"
+EC2_PUBLIC_HOSTNAME="`wget -q -O - http://169.254.169.254/latest/meta-data/public-hostname`"
+
+CUSTOMER_TAG="`aws ec2 describe-tags --output text --region \"$EC2_REGION\" --filters \"Name=key,Values=customername\" \"Name=resource-id,Values=$EC2_INSTANCE_ID\"`"
+
+CUSTOMER="`echo $CUSTOMER_TAG | cut -d' ' -f 5`"
 
 if [ -z $CUSTOMER ]; then
   CUSTOMER=$#
@@ -34,11 +38,24 @@ docker start mysql
 docker start influx
 docker start postgres-96-wal2json_db_1
 docker start sch315
+
+# Start Kerberos services
 sleep 10
 docker exec gifted_goldberg service kadmin start
 docker exec gifted_goldberg service krb5kdc start
-#docker start st313
+
+# Start Transformer instances after fixing hostname setting on node-3
 sleep 10
+#docker start st313
+if grep -q "training-" $EC2_PUBLIC_HOSTNAME
+then
+  NODE-3_TRANSFORMER_HOST=$EC2_PUBLIC_HOSTNAME
+else
+  NODE-3_TRANSFORMER_HOST=$EC2_PUBLIC_IP
+fi
+echo "setting node-3 transformer host property: $NODE-3_TRANSFORMER_HOST"
+docker exec nifty_hamilton sed -i 's#replaceme#$NODE-3_TRANSFORMER_HOST#g' /etc/transformer/transformer.properties
+
 docker exec festive_jones service transformer start
 docker exec nifty_hamilton service transformer start
 docker exec heuristic_sinoussi service transformer start
@@ -60,7 +77,8 @@ RANDOM=$$
 USERID=$CUSTOMER'-user'$((1 + $RANDOM % 100))
 echo "Creating new user $USERID..."
 PASSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-sudo useradd -G docker -m -s /bin/bash $USERID
+echo "passord: $PASSWD"
+sudo useradd -aG docker -m -s /bin/bash $USERID
 echo $USERID:$PASSWD | sudo chpasswd
 echo "Created user $USERID with password: $PASSWD"
 

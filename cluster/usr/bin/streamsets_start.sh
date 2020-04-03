6bedc2171501bc3079a74e1d555fce28c4517fc4
Ubuntu <ubuntu@ip-172-31-15-212.us-west-2.compute.internal>
2020-04-03 21:33:20 +0000

    Forgot to include one updated script

--- cluster/usr/bin/streamsets_start.sh
+++ cluster/usr/bin/streamsets_start.sh
@@ -1,4 +1,4 @@
 git -C ~ubuntu/academy-devops pull https://github.com/streamsets/academy-devops.git
-cp ~ubuntu/academy-devops/cluster/home/ubuntu/*.sh ~ubuntu
+cp ~ubuntu/academy-devops/cluster/home/ubuntu/* ~ubuntu
 ~ubuntu/start_docker.sh
 exit 0

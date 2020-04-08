be3873afc53a4b40f4b8fb25c566a375e6237723
Ubuntu <ubuntu@ip-172-31-15-212.us-west-2.compute.internal>
2020-04-08 18:37:04 +0000

    modified restart service python script to stop and start services based on an enumerated list of services

--- cluster/home/ubuntu/restart_cm_hosts.py
+++ cluster/home/ubuntu/restart_cm_hosts.py
@@ -23,12 +23,39 @@ for attempt in range(1,30):
         time.sleep(10)
         pass
 
-hosts = cluster.list_hosts()
+cms = api.get_cloudera_manager().get_service()
 
-for s in cluster.get_all_services():
-  print "Restarting roles for services ",s.name
+cluster_services_stop_order = "spark2_on_yarn,hue,oozie,impala,hive,spark_on_yarn,yarn,ks_indexer,solr,kafka,hbase,hdfs,zookeeper,streamsets,kudu"
+
+print "stopping Cloudera Manager Service"
+cms.stop()
+
+for s in cluster_services_stop_order.split(","):
+  print "stopping roles for services ",s
   sys.stdout.flush()
-  for r in s.get_all_roles():
-    s.restart_roles(r.name)
+  svc = cluster.get_service(s)
+  for r in svc.get_all_roles():
+    svc.stop_roles(r.name)
+
+zookeeper_svc = cluster.get_service("zookeeper")
+kafka_svc = cluster.get_service("kafka")
+impala_svc = cluster.get_service("impala")
+while ((len(zookeeper_svc.get_commands()) + len(kafka_svc.get_commands()) + len(impala_svc.get_commands())) > 0):
+  print "waiting 10 seconds for services to shut down..."
+  time.sleep(10)
+
+# wait another 10 seconds for good measure
+print "waiting 10 seconds for services to shut down..."
+time.sleep(10)
+
+for s in reversed(cluster_services_stop_order.split(",")):
+  print "starting roles for services ",s
+  sys.stdout.flush()
+  svc = cluster.get_service(s)
+  for r in svc.get_all_roles():
+    svc.start_roles(r.name)
+
+print "Starting Cloudera Manager Service"
+cms.start()
 
 print "Done restarting roles"
